0f9491c9c380814a999b5b59b20a72b5ce25391e
Adil Saifi <saifiadil1600x@gmail.com>
2026-03-15 10:58:03 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-black text-gray-400 text-center py-4">
-        © {new Date().getFullYear()} Shine Tensile Structure – Tensile Structure Manufacturer & Supplier in India
-
-      </footer>
-    );
-  }
-  
